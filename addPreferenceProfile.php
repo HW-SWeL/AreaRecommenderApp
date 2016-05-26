b707ccf1685c0ac1749bcd88f60a88f9f96f6b75
Alasdair Gray <A.J.G.Gray@hw.ac.uk>
2016-05-26 16:28:11 +0100

    Refactored write to file code

--- addPreferenceProfile.php
+++ addPreferenceProfile.php
@@ -16,13 +16,12 @@ $preference = array(
 if (file_exists($filename)) {
 // 	error_log("addPreferenceProfile: File exists");
 	$json = file_get_contents($filename);
-	$data = json_decode($json);
-	array_push($data, $preference);
-	file_put_contents($filename, json_encode($data));
+	$preferences = json_decode($json);
+	array_push($preferences, $preference);
 } else {
 // 	error_log("addPreferenceProfile: File does not exist");
 	$preferences[] = $preference;
-	file_put_contents($filename, json_encode($preferences));
 }
+file_put_contents($filename, json_encode($preferences));
 
 ?>
